85f9dae6e669ab84048e2d3669850fa5ab3099e3
Thuandepzai2006 <24127551@student.hcmus.edu.vn>
2025-08-29 11:58:29 +0700

    Draw Menu

---
+++ source/GameMain/Menu/src/MenuItem.cpp
@@ -0,0 +1,48 @@
+#include "../include/Menu.h"
+#include <cmath>
+
+MenuItem::MenuItem(const std::string &text, const sf::Font &font, unsigned int size, sf::Vector2f pos)
+    : m_text(font, text, size)
+{
+    m_text.setFillColor(sf::Color::White);
+
+    // setup background
+    m_background.setSize({200.f, 50.f});
+    m_background.setFillColor(sf::Color(0, 0, 0, 150));
+    m_background.setOrigin(m_background.getSize() / 2.f);
+
+    // center text
+    m_background.setPosition(pos);
+    sf::FloatRect bounds = m_text.getLocalBounds();
+    m_text.setOrigin({bounds.position.x + bounds.size.x / 2.f,
+                      bounds.position.y + bounds.size.y / 2.f});
+    m_text.setPosition(m_background.getPosition());
+}
+
+void MenuItem::setSelected(bool selected)
+{
+    m_isSelected = selected;
+    if (!selected)
+    {
+        m_background.setScale({1.f, 1.f});
+        m_background.setFillColor(sf::Color(0, 0, 0, 150));
+    }
+}
+
+void MenuItem::update(float dt)
+{
+    if (m_isSelected)
+    {
+        m_animationTime += dt * 3.f;
+        float scale = 1.f + 0.05f * std::sin(m_animationTime * 3.14f);
+        m_background.setScale({scale, scale});
+
+        m_background.setFillColor(sf::Color(255, 255, 255, 180));
+    }
+}
+
+void MenuItem::draw(sf::RenderWindow &window)
+{
+    window.draw(m_background);
+    window.draw(m_text);
+}
