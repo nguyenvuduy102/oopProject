1b3bcea8d1ee2309951dde4d2da4c0aee7032744
Duỹ <24127355@student.hcmus.edu.vn>
2025-08-29 16:01:01 +0700

    Add comment to all current file

--- source/GameMain/Core/src/GameFactory.cpp
+++ source/GameMain/Core/src/GameFactory.cpp
@@ -1,24 +1,28 @@
 #include "../include/GameFactory.h"
 #include "../include/BaseGame.h"
 
+// Create a game based on given ID
 std::unique_ptr<IGame> GameFactory::create(GameId id)
 {
+    // Pick which game to make based on ID
     switch (id)
     {
-    case TETRIS:
-        return std::make_unique<BaseGame>("Tetris", sf::Color(20, 20, 80));
-    case SNAKE:
-        return std::make_unique<BaseGame>("Snake", sf::Color(20, 80, 20));
-    case PONG:
-        return std::make_unique<BaseGame>("Pong", sf::Color(80, 20, 20));
-    case FLAPPY:
-        return std::make_unique<BaseGame>("Flappy", sf::Color(80, 80, 20));
-    default:
-        return nullptr;
+    case TETRIS:                                                            // If Tetris picked
+        return std::make_unique<BaseGame>("Tetris", sf::Color(20, 20, 80)); // Make Tetris with dark blue background
+    case SNAKE:                                                             // If Snake picked
+        return std::make_unique<BaseGame>("Snake", sf::Color(20, 80, 20));  // Make Snake with green background
+    case PONG:                                                              // If Pong picked
+        return std::make_unique<BaseGame>("Pong", sf::Color(80, 20, 20));   // Make Pong with red background
+    case FLAPPY:                                                            // If Flappy Bird picked
+        return std::make_unique<BaseGame>("Flappy", sf::Color(80, 80, 20)); // Make Flappy with yellowish background
+    default:                                                                // If ID don’t match nothing
+        return nullptr;                                                     // Return nothing, no game made
     }
 }
 
+// Get list of game names
 std::vector<std::string> GameFactory::gameNames()
 {
+    // Return array of all game names we got
     return {"Tetris", "Snake", "Pong", "Flappy Bird"};
 }
