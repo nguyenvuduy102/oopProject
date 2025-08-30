d48b8cb87604509914e6ed429be982ac7f786bda
Duỹ <24127355@student.hcmus.edu.vn>
2025-08-30 08:32:51 +0700

    Add comment to .cpp file in flappybird

--- source/ShapedGames/FlappyBird/src/AudioManager.cpp
+++ source/ShapedGames/FlappyBird/src/AudioManager.cpp
@@ -3,23 +3,32 @@
 
 namespace Flappy
 {
+    // Constructor, setup sound with buffers
     AudioManager::AudioManager() : m_flapSound(m_flapBuffer), m_hitSound(m_hitBuffer), m_scoreSound(m_scoreBuffer) {}
 
+    // Load sound files
     void AudioManager::load()
     {
+        // Try load flap sound, crash if fail
         if (!m_flapBuffer.loadFromFile("../source/assets/sounds/flap.wav"))
             throw std::runtime_error("failed to load flap.wav");
+        // Try load score sound
         if (!m_scoreBuffer.loadFromFile("../source/assets/sounds/FlappyScore.wav"))
             throw std::runtime_error("failed to load score.wav");
+        // Try load hit sound
         if (!m_hitBuffer.loadFromFile("../source/assets/sounds/crash.wav"))
             throw std::runtime_error("failed to load crash.wav");
 
+        // Set buffers to sounds
         m_flapSound.setBuffer(m_flapBuffer);
         m_scoreSound.setBuffer(m_scoreBuffer);
         m_hitSound.setBuffer(m_hitBuffer);
     }
 
+    // Play flap sound
     void AudioManager::playFlap() { m_flapSound.play(); }
+    // Play score sound
     void AudioManager::playScore() { m_scoreSound.play(); }
+    // Play hit sound
     void AudioManager::playHit() { m_hitSound.play(); }
 }
--- source/ShapedGames/FlappyBird/src/TextureManager.cpp
+++ source/ShapedGames/FlappyBird/src/TextureManager.cpp
@@ -3,20 +3,26 @@
 
 namespace Flappy
 {
+    // Constructor, do nothing yet
     TextureManager::TextureManager() {}
 
+    // Load all textures
     void TextureManager::load()
     {
+        // Try load bird textures, crash if fail
         if (!m_bird[0].loadFromFile("../source/assets/images/flappy1.png"))
             throw std::runtime_error("failed to load flappy1.png");
         if (!m_bird[1].loadFromFile("../source/assets/images/flappy2.png"))
             throw std::runtime_error("failed to load flappy2.png");
         if (!m_bird[2].loadFromFile("../source/assets/images/flappy3.png"))
             throw std::runtime_error("failed to load flappy3.png");
+        // Load pipe texture
         if (!m_pipe.loadFromFile("../source/assets/images/pipe.png"))
             throw std::runtime_error("failed to load pipe.png");
+        // Load background texture
         if (!m_background.loadFromFile("../source/assets/images/background.png"))
             throw std::runtime_error("failed to load background.png");
+        // Load game over texture
         if (!m_gameover.loadFromFile("../source/assets/images/gameover.png"))
             throw std::runtime_error("failed to load gameover.png");
     }
